39bbaf95c6576c89116cd76a71f5aabe10c2e78f
Rui Xi <xr@berkeley.edu>
2021-04-06 22:43:55 -0700

    Update matrix.c

--- src/matrix.c
+++ src/matrix.c
@@ -138,7 +138,7 @@ double get(matrix *mat, int row, int col) {
  */
 void set(matrix *mat, int row, int col, double val) {
     /* TODO: YOUR CODE HERE */
-    printf("%d, %d, %f, %d, %s", row, col, val, mat->rows, mat->data);
+    printf("%d, %d, %f, %d, %p", row, col, val, mat->rows, mat->data);
     mat->data[(mat->cols)*row + col] = val;
 }
 
