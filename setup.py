b45fad79fe9b638863336dcef792097b72a58dd1
Rui Xi <xr@berkeley.edu>
2021-04-16 17:04:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ def main():
     # Use the setup function we imported and set up the modules.
     # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
     # TODO: YOUR CODE HERE
-    module1 = Extension('numc',
-                        sources=['./src/numc.c', './src/matrix.c'])
+    module1 = Extension(name='numc',
+                        sources=['./src/numc.c', './src/matrix.c'],
+                        extra_compile_args=CFLAGS)
 
     setup(name='numc',
           version='1.0',
