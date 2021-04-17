4dbedd50485e9304b66d7a5d0a3d29e6a88748aa
Rui Xi <xr@berkeley.edu>
2021-04-17 16:00:34 -0700

    a

--- tests/unittests/unittests.py
+++ tests/unittests/unittests.py
@@ -99,26 +99,25 @@ class TestMul(TestCase):
         # TODO: YOUR CODE HERE
         dp_mat1, nc_mat1 = rand_dp_nc_matrix(2, 3, high=10, seed=0)
         dp_mat2, nc_mat2 = rand_dp_nc_matrix(3, 4, high=10, seed=1)
-        print(dp_mat1, dp_mat2)
         is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
         self.assertTrue(is_correct)
         print_speedup(speed_up)
 
     def test_medium_mul(self):
         # TODO: YOUR CODE HERE
-        # dp_mat1, nc_mat1 = rand_dp_nc_matrix(100, 200, seed=0)
-        # dp_mat2, nc_mat2 = rand_dp_nc_matrix(200, 300, seed=1)
-        # is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
-        # self.assertTrue(is_correct)
-        # print_speedup(speed_up)
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(100, 200, seed=0)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(200, 300, seed=1)
+        is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
+        self.assertTrue(is_correct)
+        print_speedup(speed_up)
         pass
     def test_large_mul(self):
         # TODO: YOUR CODE HERE
-        # dp_mat1, nc_mat1 = rand_dp_nc_matrix(200, 200, seed=0)
-        # dp_mat2, nc_mat2 = rand_dp_nc_matrix(200, 200, seed=1)
-        # is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
-        # self.assertTrue(is_correct)
-        # print_speedup(speed_up)
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(200, 300, seed=0)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(300, 400, seed=1)
+        is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
+        self.assertTrue(is_correct)
+        print_speedup(speed_up)
         pass
 class TestPow(TestCase):
     def test_small_pow(self):
