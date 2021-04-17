5806da7a81083267ca64e609284615f021aa5f90
Rui Xi <xr@berkeley.edu>
2021-04-17 15:36:14 -0700

    Update unittests.py

--- tests/unittests/unittests.py
+++ tests/unittests/unittests.py
@@ -111,7 +111,7 @@ class TestMul(TestCase):
         # is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
         # self.assertTrue(is_correct)
         # print_speedup(speed_up)
-
+        pass
     def test_large_mul(self):
         # TODO: YOUR CODE HERE
         # dp_mat1, nc_mat1 = rand_dp_nc_matrix(200, 200, seed=0)
@@ -119,7 +119,7 @@ class TestMul(TestCase):
         # is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
         # self.assertTrue(is_correct)
         # print_speedup(speed_up)
-
+        pass
 class TestPow(TestCase):
     def test_small_pow(self):
         # TODO: YOUR CODE HERE
