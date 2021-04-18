fff7e246c23751a64262d9071001ebe6fb610a6c
Rui Xi <xr@berkeley.edu>
2021-04-18 12:35:05 -0700

    Update unittests.py

--- tests/unittests/unittests.py
+++ tests/unittests/unittests.py
@@ -106,8 +106,8 @@ class TestMul(TestCase):
 
     def test_medium_mul(self):
         # TODO: YOUR CODE HERE
-        dp_mat1, nc_mat1 = rand_dp_nc_matrix(100, 200, seed=0)
-        dp_mat2, nc_mat2 = rand_dp_nc_matrix(200, 300, seed=1)
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(10, 20, seed=0)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(20, 30, seed=1)
         is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
         self.assertTrue(is_correct)
         print_speedup(speed_up)
