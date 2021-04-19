4c1f2ee7165c55684f0d7bd494cc3ec650f4ec8f
Rui Xi <xr@berkeley.edu>
2021-04-18 21:47:59 -0700

    Update matrix.c

--- src/matrix.c
+++ src/matrix.c
@@ -200,8 +200,7 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
 //    }
 //    return 0;
     int length_floor8 = (mat1->rows)*(mat1->cols) / 8 * 8;
-    #pragma omp parallel
-    {
+
         #pragma omp for
         for(unsigned int i = 0; i < length_floor8; i += 8) {
             __m256d m1 =  _mm256_loadu_pd(mat1->data + i);
@@ -211,7 +210,7 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
             _mm256_storeu_pd(result->data + i, _mm256_add_pd(m1, m2));
             _mm256_storeu_pd(result->data + i + 4, _mm256_add_pd(m3, m4));
         }
-    }
+    
     #pragma omp for
     for (int i = (mat1->rows)*(mat1->cols) / 8 * 8; i < (mat1->rows)*(mat1->cols); i++) {
         result->data[i] = mat1->data[i] + mat2->data[i];
