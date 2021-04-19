3c2aa8921405cd8b524e24a8ac479af8a4973fad
Rui Xi <xr@berkeley.edu>
2021-04-18 21:09:05 -0700

    Update matrix.c

--- src/matrix.c
+++ src/matrix.c
@@ -354,13 +354,13 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
         }
     }
 ////debug:
-    for (int i = 0; i < mat2->cols; i++) {
-        printf("\n");
-        for (int j = 0; j < mat2->rows; j++) {
-    		printf("%lf,", m2trans[(i * m2t_cols) + j]);
-    	}
-    	printf("\n");
-    }
+//    for (int i = 0; i < mat2->cols; i++) {
+//        printf("\n");
+//        for (int j = 0; j < mat2->rows; j++) {
+//    		printf("%lf,", m2trans[(i * m2t_cols) + j]);
+//    	}
+//    	printf("\n");
+//    }
 
 
 // computation:
@@ -415,7 +415,7 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
             }
             *(result->data + i * result_cols + j) = sum;
 
-            
+
             ////debug:
 //            for (int i = 0; i < mat1->rows; i++) {
 //                printf("\n");
