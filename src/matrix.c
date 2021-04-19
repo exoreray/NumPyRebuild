c9f5971b792da1c37a52ddfb4a3f0d272ae2f18b
Rui Xi <xr@berkeley.edu>
2021-04-19 13:12:09 -0700

    Update matrix.c

--- src/matrix.c
+++ src/matrix.c
@@ -203,7 +203,7 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     #pragma omp parallel
     {
         #pragma omp for
-        for(unsigned int i = 0; i < length_floor8; i += 16) {
+        for(unsigned int i = 0; i < length_floor8; i += 8) {
             __m256d m1 =  _mm256_loadu_pd(mat1->data + i);
             __m256d m3 =  _mm256_loadu_pd(mat1->data + i + 4);
             __m256d m2 =  _mm256_loadu_pd(mat2->data + i);
@@ -232,18 +232,22 @@ int sub_matrix(matrix *result, matrix *mat1, matrix *mat2) {
 //        result->data[i] = mat1->data[i]-mat2->data[i];
 //    }
 //    return 0;
-    int length_floor16 = (mat1->rows)*(mat1->cols) / 16 * 16;
+    int length_floor16 = (mat1->rows)*(mat1->cols) / 32 * 32;
 #pragma omp parallel
     {
 #pragma omp for
-        for(unsigned int i = 0; i < length_floor16; i += 16) {
+        for(unsigned int i = 0; i < length_floor16; i += 32) {
             _mm256_storeu_pd(result->data + i, _mm256_sub_pd(_mm256_loadu_pd(mat1->data + i), _mm256_loadu_pd(mat2->data + i)));
             _mm256_storeu_pd(result->data + i + 4, _mm256_sub_pd(_mm256_loadu_pd(mat1->data + i + 4), _mm256_loadu_pd(mat2->data + i + 4)));
             _mm256_storeu_pd(result->data + i + 8, _mm256_sub_pd(_mm256_loadu_pd(mat1->data + i + 8), _mm256_loadu_pd(mat2->data + i + 8)));
             _mm256_storeu_pd(result->data + i + 12, _mm256_sub_pd(_mm256_loadu_pd(mat1->data + i + 12), _mm256_loadu_pd(mat2->data + i + 12)));
+            _mm256_storeu_pd(result->data + i + 16, _mm256_sub_pd(_mm256_loadu_pd(mat1->data + i + 16), _mm256_loadu_pd(mat2->data + i)));
+            _mm256_storeu_pd(result->data + i + 20, _mm256_sub_pd(_mm256_loadu_pd(mat1->data + i + 20), _mm256_loadu_pd(mat2->data + i + 4)));
+            _mm256_storeu_pd(result->data + i + 24, _mm256_sub_pd(_mm256_loadu_pd(mat1->data + i + 24), _mm256_loadu_pd(mat2->data + i + 8)));
+            _mm256_storeu_pd(result->data + i + 28, _mm256_sub_pd(_mm256_loadu_pd(mat1->data + i + 28), _mm256_loadu_pd(mat2->data + i + 12)));
         }
     }
-    for (int i = (mat1->rows)*(mat1->cols) / 16 * 16; i < (mat1->rows)*(mat1->cols); i++) {
+    for (int i = (mat1->rows)*(mat1->cols) / 32 * 32; i < (mat1->rows)*(mat1->cols); i++) {
         result->data[i] = mat1->data[i] - mat2->data[i];
     }
     return 0;
